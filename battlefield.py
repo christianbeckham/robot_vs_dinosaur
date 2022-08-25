57c95451a4503d659ef536b198a06ae14f8b9f3f
Christian Beckham <urenacb22@gmail.com>
2022-08-25 11:19:16 -0500

    feat: setup main.py and created classes for each module

---
+++ battlefield.py
@@ -0,0 +1,19 @@
+from robot import Robot
+from dinosaur import Dinosaur
+
+class Battlefield:
+    def __init__(self):
+        self.robot = Robot('Optimus Prime')
+        self.dinosaur = Dinosaur('Godzilla', 30)
+    
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
---
+++ robot.py
@@ -0,0 +1,11 @@
+from weapon import Weapon
+
+
+class Robot:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.active_weapon = Weapon('Blaster', 25)
+
+    def attack(self, dinosaur):
+        pass
